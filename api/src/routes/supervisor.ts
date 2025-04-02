c781e51d30876d22cf8b63e4bee0d5f22e59de47
Gabriel Ramos <luisgabrielramosrobles@gmail.com>
2025-04-02 14:51:11 -0400

    typo

--- api/src/routes/supervisor.ts
+++ api/src/routes/supervisor.ts
@@ -100,7 +100,7 @@ router.get(
       const end = new Date(parseInt(endDate));
 
       const tickets = await Ticket.findAll({
-        attributes: ["id", "closing_state", "isClosed", "createdAt"],
+        attributes: ["id", "closing_state", "isOpen", "createdAt"],
         where: {
           [Op.or]: {
             isOpen: true,
